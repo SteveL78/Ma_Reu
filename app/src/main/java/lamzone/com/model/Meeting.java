332a8cabeac24e3fb50dd87126e5c4ceef37660d
steveleroy <casaleroy@gmail.com>
2020-10-31 11:32:50 +0100

    fix tests

--- app/src/main/java/lamzone/com/model/Meeting.java
+++ app/src/main/java/lamzone/com/model/Meeting.java
@@ -41,6 +41,9 @@ public class Meeting implements Serializable {
 
     }
 
+    public long getId() {
+        return id;
+    }
 
     public void setId(long id) {
         this.id = id;
--- app/src/main/java/lamzone/com/service/DummyMeetingGenerator.java
+++ app/src/main/java/lamzone/com/service/DummyMeetingGenerator.java
@@ -17,18 +17,18 @@ public abstract class DummyMeetingGenerator {
 
     // Création de la liste des réunions avec informations
     public static List<Meeting> DUMMY_MEETINGS = Arrays.asList(
-            new Meeting(791, "Réunion A", getDate("15/01/2020 12:55"), getDate("10/05/2020 13:55"), RoomGenerator.DUMMY_ROOMS.get(0), ParticipantGenerator.DUMMY_PARTICIPANTS),
-            new Meeting(226, "Réunion B", getDate("15/01/2020 08:00"), getDate("10/05/2020 08:30"), RoomGenerator.DUMMY_ROOMS.get(0), Collections.singletonList(ParticipantGenerator.DUMMY_PARTICIPANTS.get(0))),
-            new Meeting(453, "Réunion C", getDate("27/01/2020 16:00"), getDate("10/05/2020 16:30"), RoomGenerator.DUMMY_ROOMS.get(2), Collections.singletonList(ParticipantGenerator.DUMMY_PARTICIPANTS.get(2))),
-            new Meeting(45, "Réunion B", getDate("27/01/2020 09:00"), getDate("10/05/2020 09:45"), RoomGenerator.DUMMY_ROOMS.get(1), Collections.singletonList(ParticipantGenerator.DUMMY_PARTICIPANTS.get(1))),
-            new Meeting(511, "Réunion D", getDate("24/03/2020 13:00"), getDate("10/05/2020 13:55"), RoomGenerator.DUMMY_ROOMS.get(3), Collections.singletonList(ParticipantGenerator.DUMMY_PARTICIPANTS.get(3))),
-            new Meeting(62, "Réunion A", getDate("24/03/2020 10:00"), getDate("10/05/2020 10:45"), RoomGenerator.DUMMY_ROOMS.get(5), Collections.singletonList(ParticipantGenerator.DUMMY_PARTICIPANTS.get(4))),
-            new Meeting(97, "Réunion A", getDate("22/05/2020 15:00"), getDate("10/05/2020 15:37"), RoomGenerator.DUMMY_ROOMS.get(4), Collections.singletonList(ParticipantGenerator.DUMMY_PARTICIPANTS.get(5))),
-            new Meeting(898, "Réunion E", getDate("13/08/2020 11:00"), getDate("10/05/2020 11:45"), RoomGenerator.DUMMY_ROOMS.get(6), Collections.singletonList(ParticipantGenerator.DUMMY_PARTICIPANTS.get(7))),
-            new Meeting(94, "Réunion C", getDate("12/09/2020 18:00"), getDate("10/05/2020 18:45"), RoomGenerator.DUMMY_ROOMS.get(8), Collections.singletonList(ParticipantGenerator.DUMMY_PARTICIPANTS.get(6))),
-            new Meeting(100, "Réunion D", getDate("12/09/2020 17:00"), getDate("10/05/2020 17:45"), RoomGenerator.DUMMY_ROOMS.get(7), Collections.singletonList(ParticipantGenerator.DUMMY_PARTICIPANTS.get(1))),
-            new Meeting(625, "Réunion E", getDate("24/12/2020 12:00"), getDate("10/05/2020 12:45"), RoomGenerator.DUMMY_ROOMS.get(8), Collections.singletonList(ParticipantGenerator.DUMMY_PARTICIPANTS.get(3))),
-            new Meeting(1200, "Réunion C", getDate("31/12/2020 14:00"), getDate("05/07/2020 14:45"), RoomGenerator.DUMMY_ROOMS.get(9), Collections.singletonList(ParticipantGenerator.DUMMY_PARTICIPANTS.get(4)))
+            new Meeting(791, "Réunion A", getDate("15/01/2020 12:55"), getDate("15/01/2020 13:55"), RoomGenerator.DUMMY_ROOMS.get(0), ParticipantGenerator.DUMMY_PARTICIPANTS),
+            new Meeting(226, "Réunion B", getDate("15/01/2020 08:00"), getDate("15/01/2020 08:30"), RoomGenerator.DUMMY_ROOMS.get(0), Collections.singletonList(ParticipantGenerator.DUMMY_PARTICIPANTS.get(0))),
+            new Meeting(453, "Réunion C", getDate("27/01/2020 16:00"), getDate("27/01/2020 16:30"), RoomGenerator.DUMMY_ROOMS.get(2), Collections.singletonList(ParticipantGenerator.DUMMY_PARTICIPANTS.get(2))),
+            new Meeting(45, "Réunion B", getDate("27/01/2020 09:00"), getDate("27/01/2020 09:45"), RoomGenerator.DUMMY_ROOMS.get(1), Collections.singletonList(ParticipantGenerator.DUMMY_PARTICIPANTS.get(1))),
+            new Meeting(511, "Réunion D", getDate("24/03/2020 13:00"), getDate("24/03/2020 13:55"), RoomGenerator.DUMMY_ROOMS.get(3), Collections.singletonList(ParticipantGenerator.DUMMY_PARTICIPANTS.get(3))),
+            new Meeting(62, "Réunion A", getDate("24/03/2020 10:00"), getDate("24/03/2020 10:45"), RoomGenerator.DUMMY_ROOMS.get(5), Collections.singletonList(ParticipantGenerator.DUMMY_PARTICIPANTS.get(4))),
+            new Meeting(97, "Réunion A", getDate("22/05/2020 15:00"), getDate("22/05/2020 15:37"), RoomGenerator.DUMMY_ROOMS.get(4), Collections.singletonList(ParticipantGenerator.DUMMY_PARTICIPANTS.get(5))),
+            new Meeting(898, "Réunion E", getDate("13/08/2020 11:00"), getDate("13/08/2020 11:45"), RoomGenerator.DUMMY_ROOMS.get(6), Collections.singletonList(ParticipantGenerator.DUMMY_PARTICIPANTS.get(7))),
+            new Meeting(94, "Réunion C", getDate("12/09/2020 18:00"), getDate("13/08/2020 18:45"), RoomGenerator.DUMMY_ROOMS.get(8), Collections.singletonList(ParticipantGenerator.DUMMY_PARTICIPANTS.get(6))),
+            new Meeting(100, "Réunion D", getDate("12/09/2020 17:00"), getDate("12/092020 17:45"), RoomGenerator.DUMMY_ROOMS.get(7), Collections.singletonList(ParticipantGenerator.DUMMY_PARTICIPANTS.get(1))),
+            new Meeting(625, "Réunion E", getDate("24/12/2020 12:00"), getDate("24/12/2020 12:45"), RoomGenerator.DUMMY_ROOMS.get(8), Collections.singletonList(ParticipantGenerator.DUMMY_PARTICIPANTS.get(3))),
+            new Meeting(1200, "Réunion C", getDate("31/12/2020 14:00"), getDate("31/12/2020 14:45"), RoomGenerator.DUMMY_ROOMS.get(9), Collections.singletonList(ParticipantGenerator.DUMMY_PARTICIPANTS.get(4)))
     );
 
     private static Date getDate(String dateStr) {
